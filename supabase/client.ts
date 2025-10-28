15d370cb70bc33af43608504ae865aec1adb2a15
jesus <usajesuscash@gmail.com>
2025-10-28 16:53:10 -0500

    mejora supa

--- supabase/client.ts
+++ supabase/client.ts
@@ -1,13 +1,6 @@
 import { createClient } from "@supabase/supabase-js";
 
-const supabaseUrl = process.env.NEXT_PUBLIC_PROJECT_URL_SUPABASE;
-const supabaseKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY;
+const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL!;
+const supabaseKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!;
 
-if (!supabaseUrl || !supabaseKey) {
-  console.warn("⚠️ Variables de entorno Supabase no configuradas.");
-}
-
-export const supabase =
-  supabaseUrl && supabaseKey
-    ? createClient(supabaseUrl, supabaseKey)
-    : ({} as ReturnType<typeof createClient>);
+export const supabase = createClient(supabaseUrl, supabaseKey);
