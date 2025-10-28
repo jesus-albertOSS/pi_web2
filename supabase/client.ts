55aa7659e64372f778f25f03fb4db27e1d1d60b9
jesus <usajesuscash@gmail.com>
2025-10-28 16:47:28 -0500

    areglo supabase

--- supabase/client.ts
+++ supabase/client.ts
@@ -1,8 +1,13 @@
 import { createClient } from "@supabase/supabase-js";
 
-
 const supabaseUrl = process.env.NEXT_PUBLIC_PROJECT_URL_SUPABASE;
 const supabaseKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY;
 
+if (!supabaseUrl || !supabaseKey) {
+  console.warn("⚠️ Variables de entorno Supabase no configuradas.");
+}
 
-export const supabase = createClient(supabaseUrl, supabaseKey);
+export const supabase =
+  supabaseUrl && supabaseKey
+    ? createClient(supabaseUrl, supabaseKey)
+    : ({} as ReturnType<typeof createClient>);
